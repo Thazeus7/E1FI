7af3964784d1b8bbf3e6fd4072ec562251a5c14b
Pierre Fuchs <pierre.fuchs@bbw-moshd.de>
2022-10-13 09:21:25 +0200

    Initial commit

---
+++ src/Datentypen.java
@@ -0,0 +1,109 @@
+public class Datentypen {
+
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------");
+        System.out.println("byte");
+        System.out.println(Byte.BYTES);
+        System.out.println("-----------------------------------");
+ byte a1 = 0;
+ byte a2 = Byte.MAX_VALUE ;
+ byte a3 = Byte.MIN_VALUE;
+ byte a4 = (byte)(a2 + 1);
+byte a5 = (byte) (a3 - 1);
+ System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+
+        System.out.println("//////////////////////////////////////");
+
+        System.out.println("-----------------------------------");
+        System.out.println("Short");
+        System.out.println(Short.BYTES);
+        System.out.println("-----------------------------------");
+        short Fn = 0;
+   short Fn1 = Short.MAX_VALUE ;
+       short Fn2 = Short.MIN_VALUE;
+        short Fn3 = (short)(Fn1 + 1);
+        short Fn4 = (short) (Fn2 - 1);
+        System.out.println(Fn);
+        System.out.println(Fn1);
+        System.out.println(Fn2);
+        System.out.println(Fn3);
+        System.out.println(Fn4);
+
+
+        System.out.println("//////////////////////////////////////");
+
+        System.out.println("-----------------------------------");
+        System.out.println("int");
+        System.out.println(Integer.BYTES);
+        System.out.println("-----------------------------------");
+        int In = 0;
+        int In1 = Integer.MAX_VALUE;
+        int In2 = Integer.MIN_VALUE;
+        int In3 = (int)(In1 + 1);
+        int In4 = (int) (In2 - 1);
+        System.out.println(In);
+        System.out.println(In1);
+        System.out.println(In2);
+        System.out.println(In3);
+        System.out.println(In4);
+
+        System.out.println("//////////////////////////////////////");
+
+        System.out.println("-----------------------------------");
+        System.out.println("float");
+        System.out.println(Float.BYTES);
+        System.out.println("-----------------------------------");
+        float fn = 0;
+        float fn1 = Float.MAX_VALUE;
+        float fn2 = Float.MIN_VALUE;
+        float fn3 = (float)(fn1 + 1);
+        float fn4 = (float) (fn2 - 1);
+        System.out.println(fn);
+        System.out.println(fn1);
+        System.out.println(fn2);
+        System.out.println(fn3);
+        System.out.println(fn4);
+
+        System.out.println("//////////////////////////////////////");
+        System.out.println("-----------------------------------");
+        System.out.println("long");
+        System.out.println(Long.BYTES);
+        System.out.println("-----------------------------------");
+        long ln = 0;
+        long ln1 = Long.MAX_VALUE;
+        long ln2 = Long.MIN_VALUE;
+        long ln3 = (long)(fn1 + 1);
+        long ln4 = (long) (fn2 - 1);
+        System.out.println(ln);
+        System.out.println(ln1);
+        System.out.println(ln2);
+        System.out.println(ln3);
+        System.out.println(ln4);
+
+        System.out.println("//////////////////////////////////////");
+
+        System.out.println("-----------------------------------");
+        System.out.println("double");
+        System.out.println(Double.BYTES);
+        System.out.println("-----------------------------------");
+        double lln = 0;
+        double lln1 = Double.MAX_VALUE;
+        double lln2 = Double.MIN_VALUE;
+        double lln3 = (double) (fn1 + 1);
+        double lln4 = (double) (fn2 - 1);
+        System.out.println(lln);
+        System.out.println(lln1);
+        System.out.println(lln2);
+        System.out.println(lln3);
+        System.out.println(lln4);
+
+
+
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world! " + "was geht louser "+  args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[0]);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
